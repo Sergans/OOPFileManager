109d6bc6634800da3b9dd5b50d3e6b38c32f3571
Сергей <gan7er@yandex.ru>
2021-11-16 18:41:03 +0300

    begin

---
+++ Lesson-8FileManager/Lesson-8FileManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_8FileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
