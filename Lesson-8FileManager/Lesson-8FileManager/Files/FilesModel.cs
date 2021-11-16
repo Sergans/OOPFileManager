d03c35d219b6099df840ffaa3047f1a9aa3f1c6c
Сергей <gan7er@yandex.ru>
2021-11-16 21:25:30 +0300

    Проба

---
+++ Lesson-8FileManager/Lesson-8FileManager/Files/FilesModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_8FileManager.Files
+{
+   public class FilesModel
+    {
+        public string Path { get; set;}
+        public string Print()
+        {
+            return Path;
+        }
+    }
+}
--- Lesson-8FileManager/Lesson-8FileManager/Program.cs
+++ Lesson-8FileManager/Lesson-8FileManager/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using Lesson_8FileManager;
+using Lesson_8FileManager.Files;
+using System.Collections.Generic;
 
 namespace Lesson_8FileManager
 {
@@ -6,6 +10,29 @@ namespace Lesson_8FileManager
     {
         static void Main(string[] args)
         {
+            // FileInfo a = new FileInfo();
+            // File.Delete(@"C:\Users\GANS\Desktop\Catalog\2.txt");
+            //Directory.Delete(@"C:\Users\GANS\Desktop\Catalog\Catalog4");
+            //IEnumerable<string> a=Directory.(@"C:\Users\GANS\Desktop\Catalog");
+            DirectoryInfo a = new DirectoryInfo(@"C:\Users\GANS\Desktop\Catalog");
+            foreach(var z in a.GetDirectories())
+            {
+                Console.WriteLine(z.Name);
+                foreach(var v in z.GetDirectories())
+                {
+                    Console.WriteLine(v.Name);
+                    if (v.Name == "11")
+                    {
+                        v.Delete();
+                    }
+                   
+                }
+                
+            }
+
+            
+            
+
             
         }
     }
