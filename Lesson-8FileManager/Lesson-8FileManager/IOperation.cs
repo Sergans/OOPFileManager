d3497c9bb4783a2b42f1d9d5f5ce07da619d1a08
Сергей <gan7er@yandex.ru>
2021-11-16 18:55:02 +0300

    add interface

---
+++ Lesson-8FileManager/Lesson-8FileManager/IOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_8FileManager
+{
+    public interface IOperation
+    {
+        void Create();
+        void Get();
+        void Update();
+        void Delete();
+    }
+}
