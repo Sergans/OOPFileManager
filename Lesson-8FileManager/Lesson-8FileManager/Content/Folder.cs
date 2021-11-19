55b6c6f0f81fa7a9123354ad5dff98defcc90463
Сергей <gan7er@yandex.ru>
2021-11-19 22:56:49 +0300

    add folder ok

--- Lesson-8FileManager/Lesson-8FileManager/Content/Folder.cs
+++ Lesson-8FileManager/Lesson-8FileManager/Content/Folder.cs
@@ -13,7 +13,8 @@ namespace Lesson_8FileManager.Files
         public string Name { get; set; }
         public long Size { get; set; }
         public DateTime DataCreate { get; set; }
-        public List<FileModel> ListContentModel = new List<FileModel>();
+        public List<FileModel> ListFileModel = new List<FileModel>();
+        public List<Folder> ListFolderModel = new List<Folder>();
 
         public Folder(string path)
         {
@@ -22,6 +23,7 @@ namespace Lesson_8FileManager.Files
             Name = directory.Name;
             DataCreate = directory.CreationTime;
             AddFile();
+            AddFolder();
             Size = GetSize();
 
 
@@ -29,7 +31,7 @@ namespace Lesson_8FileManager.Files
         }
         public long GetSize()
         { long sum = 0;
-            foreach(var file in ListContentModel)
+            foreach(var file in ListFileModel)
             {
                 sum +=file.Size;
             }
@@ -40,18 +42,24 @@ namespace Lesson_8FileManager.Files
             var a = Directory.GetFiles(Path);
             foreach(var file in a)
             {
-                ListContentModel.Add(new FileModel(file));
+                ListFileModel.Add(new FileModel(file));
             }
-            
-
+        }
+        public void AddFolder()
+        {
+            var a = Directory.GetDirectories(Path);
+            foreach (var directory in a)
+            {
+                ListFolderModel.Add(new Folder(directory));
+            }
         }
         public FileModel Delete(FileModel content)
         {
-            for(int i = 0; i < ListContentModel.Count; i++)
+            for(int i = 0; i < ListFileModel.Count; i++)
             {
-                if (ListContentModel[i] == content)
+                if (ListFileModel[i] == content)
                 {
-                    ListContentModel.Remove(content.Delete());
+                    ListFileModel.Remove(content.Delete());
                     return content;
                 }
             }
